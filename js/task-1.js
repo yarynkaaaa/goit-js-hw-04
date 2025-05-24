5cc83006e9ff7d33a2b6a99a1678fde7c1e45e3c
yarynkaaaa <146334331+yarynkaaaa@users.noreply.github.com>
2025-05-24 22:51:01 +0300

    do hw

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,10 @@
-function isEnoughCapacity(products, containerSize) {}
+function isEnoughCapacity(products, containerSize) {
+  let totalProducts = 0;
+  for (const product of Object.values(products)) {
+    totalProducts += product;
+  }
+  return totalProducts <= containerSize;
+}
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 
 console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,22 @@
 const profile = {
-    username: "Jacob",
-    playTime: 300,
+  username: 'Jacob',
+  playTime: 300,
+
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
 };
-changeUsername(newName);
-updatePlayTime(hours);
-getInfo();
 
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-
-profile.changeUsername("Marco");
+profile.changeUsername('Marco');
 console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
 profile.updatePlayTime(20);
 console.log(profile.getInfo()); // "Marco has 320 active hours!"
