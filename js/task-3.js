d1756f7b4e3230eec35f7f712cd06de7d75bd578
yarynkaaaa <146334331+yarynkaaaa@users.noreply.github.com>
2025-05-24 15:13:14 +0300

    add tasks condition

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+};
+changeUsername(newName);
+updatePlayTime(hours);
+getInfo();
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
